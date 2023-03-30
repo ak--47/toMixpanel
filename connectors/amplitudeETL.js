dbf26daca2bfd2b55889e046f0fc48961690dbbf
AK <akrivit@gmail.com>
2023-03-30 14:42:04 -0400

    write logs to logfile

--- connectors/amplitudeETL.js
+++ connectors/amplitudeETL.js
@@ -2,80 +2,87 @@
 import dayjs from 'dayjs';
 import * as path from 'path';
 import { readdir } from 'fs/promises';
-import mpImport from 'mixpanel-import'
+import mpImport from 'mixpanel-import';
+import { touch, mkdir } from 'ak-tools';
 
 //scripts
-import amplitudeExtract from '../extract/amplitude.js'
-import amplitudeTransform from '../transform/amplitudeToMixpanel.js'
-import sendEventsToMixpanel from '../load/sendEventsToMixpanel.js'
-import sendProfilesToMixpanel from '../load/sendProfilesToMixpanel.js'
+import amplitudeExtract from '../extract/amplitude.js';
+import amplitudeTransform from '../transform/amplitudeToMixpanel.js';
+import sendEventsToMixpanel from '../load/sendEventsToMixpanel.js';
+import sendProfilesToMixpanel from '../load/sendProfilesToMixpanel.js';
 
 
 async function amplitudeETL(config, directoryName) {
-    //for auth
-    let credentials = {
-        apiKey: config.source.params.api_key,
-        apiSecret: config.source.params.api_secret
-    }
+	//for auth
+	let credentials = {
+		apiKey: config.source.params.api_key,
+		apiSecret: config.source.params.api_secret
+	};
 
-    let { start_date, end_date } = config.source.params;
-    //co-erce the dates to amp format
-    let dates = {
-        start: dayjs(start_date).format('YYYYMMDDTHH'),
-        end: dayjs(end_date).format('YYYYMMDDTHH')
-    }
+	let { start_date, end_date } = config.source.params;
+	//co-erce the dates to amp format
+	let dates = {
+		start: dayjs(start_date).format('YYYYMMDDTHH'),
+		end: dayjs(end_date).format('YYYYMMDDTHH')
+	};
 
-    let extractedData;
-    if (config.source.options?.path) {
-        //path to data already specified
-        let filesPath = path.resolve(config.source.options?.path)
-        console.log(`local path specified: ${filesPath}`)
-        let files = (await readdir(filesPath)).map(fileLoc => path.resolve(`${filesPath}/${fileLoc}`))
-        console.log(`found ${files.length} files`)
-        extractedData = files;
-    } else {
+	let extractedData;
+	if (config.source.options?.path) {
+		//path to data already specified
+		let filesPath = path.resolve(config.source.options?.path);
+		console.log(`local path specified: ${filesPath}`);
+		let files = (await readdir(filesPath)).map(fileLoc => path.resolve(`${filesPath}/${fileLoc}`));
+		console.log(`found ${files.length} files`);
+		extractedData = files;
+	} else {
 
-        console.log('EXTRACT!\n')
-        extractedData = await amplitudeExtract(credentials, dates, directoryName, config.source.options['is EU?']);
-        if (!extractedData) {
-            return false;
-        }
-    }
-    console.log('TRANSFORM!\n')
-    let transformedData = await amplitudeTransform(extractedData, `./savedData/${directoryName}`, config.destination.token);
+		console.log('EXTRACT!\n');
+		extractedData = await amplitudeExtract(credentials, dates, directoryName, config.source.options['is EU?']);
+		if (!extractedData) {
+			return false;
+		}
+	}
+	console.log('TRANSFORM!\n');
+	let transformedData = await amplitudeTransform(extractedData, `./savedData/${directoryName}`, config.destination.token);
 
-    console.log('LOAD!')
-    console.log('   events:\n')
-    let eventsDir = path.resolve(`./savedData/${directoryName}/transformed/events`);
-    let usersDir = path.resolve(`./savedData/${directoryName}/transformed/profiles`);
-    let mergeDir = path.resolve(`./savedData/${directoryName}/transformed/mergeTables`);
+	console.log('LOAD!');
+	console.log('   events:\n');
+	let eventsDir = path.resolve(`./savedData/${directoryName}/transformed/events`);
+	let usersDir = path.resolve(`./savedData/${directoryName}/transformed/profiles`);
+	let mergeDir = path.resolve(`./savedData/${directoryName}/transformed/mergeTables`);
 
-    let creds  = {
-        acct: config.destination.service_account_user,
-        pass: config.destination.service_account_pass,
-        project: config.destination.project_id,
-        token: config.destination.token
+	let creds = {
+		acct: config.destination.service_account_user,
+		pass: config.destination.service_account_pass,
+		project: config.destination.project_id,
+		token: config.destination.token
 
-    }
-    let region = (config.destination?.options['is EU?'] ? `EU` : `US`);
+	};
+	let region = (config.destination?.options['is EU?'] ? `EU` : `US`);
 
-    let importedEvents = await mpImport(creds, eventsDir, {recordType: `event`, logs: true, region, recordsPerBatch: config?.destination?.options?.recordsPerBatch || 2000 });    
-    let imporedProfiles = await mpImport(creds, usersDir, {recordType: `user`, logs: true, region, recordsPerBatch: config?.destination?.options?.recordsPerBatch || 2000 });
-    let importedMergeTables = await mpImport(creds, mergeDir, {recordType: `event`, logs: true, region, recordsPerBatch: config?.destination?.options?.recordsPerBatch || 2000 });
+	let importedEvents = await mpImport(creds, eventsDir, { recordType: `event`, logs: true, region, recordsPerBatch: config?.destination?.options?.recordsPerBatch || 2000 });
+	let importedProfiles = await mpImport(creds, usersDir, { recordType: `user`, logs: true, region, recordsPerBatch: config?.destination?.options?.recordsPerBatch || 2000 });
+	let importedMergeTables = await mpImport(creds, mergeDir, { recordType: `event`, logs: true, region, recordsPerBatch: config?.destination?.options?.recordsPerBatch || 2000 });
 
-    console.log(`\nSUMMARY:`)
-    console.log(`
+	console.log(`\nSUMMARY:`);
+	console.log(`
     ${smartCommas(importedEvents.results?.totalRecordCount)} events imported
-    ${smartCommas(imporedProfiles.results?.totalRecordCount)} users merged
-    ${smartCommas(importedMergeTables.results?.totalRecordCount)} profiles updated`)
+    ${smartCommas(importedProfiles.results?.totalRecordCount)} users merged
+    ${smartCommas(importedMergeTables.results?.totalRecordCount)} profiles updated`);
 
+	// write logs
+	const logsDir = path.resolve(`./savedData/${directoryName}/logs`);
+	await mkdir(logsDir)
+	await touch(path.resolve(`${logsDir}/eventLogs.json`), importedEvents.responses, true, false, false)
+	await touch(path.resolve(`${logsDir}/profileLogs.json`), importedProfiles.responses, true, false, false)
+	await touch(path.resolve(`${logsDir}/mergeLogs.json`), importedMergeTables.responses, true, false, false) 
 
 
 }
 
 //logging stuffs
 function smartCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 export default amplitudeETL;
